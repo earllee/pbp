c3aee563dd94b3e72c4e6fe4cc1a586f312eaf6f
earllee <earlvlee@gmail.com>
2014-11-14 00:50:36 -0500

    Finished decryption part of decryptData.

--- finalProject/src/PBP.cc
+++ finalProject/src/PBP.cc
@@ -2,48 +2,17 @@
 
 #define SYMMETRIC_KEY_SIZE 16
 
-// In: datagram of msg to send, pubKey of recipient, privKey of self
-// Returns QByteArray, datagram that represents QVariantMap containing
-// - original datagram encrypted by random sym key with AES-128 cipher
-// - sym key encrypted by recipient's public key, RSA-1028
-// - Digest of original datagram, hashed by SHA-1, signed w/ RSA-1028 own privKey
-// - Initialization vector for the AES cipher, unsecured
-QByteArray encryptDatagram(QByteArray datagram, QCA::PublicKey pubKey, 
-    QCA::PrivateKey privKey) 
-{
-    QCA::SymmetricKey symKey(16); // 128 bit random symmetric key
-    QCA::InitializationVector iV(16); // need initVector to randomize input
-    QCA::Cipher cipher(QString("aes128"), QCA::Cipher::CBC,
-        QCA::Cipher::DefaultPadding, QCA::Encode, symKey, iV);
-    QCA::MemoryRegion dataMemCpy(datagram), dataMemOut(datagram);
-    dataMemOut = cipher.update(dataMemOut);
-    dataMemOut = cipher.final();    // Encrypt datagram w/ symKey, padded
-
-    // Hash the contents of datagram
-    QByteArray dataDigest = QCA::Hash("sha1").hash(dataMemCpy).toByteArray();
-    
-    // Sign hash
-    QCA::SecureArray secureDigest;
-    privKey.decrypt(QCA::MemoryRegion(dataDigest), &secureDigest, QCA::EME_PKCS1_OAEP);
-    
-    // Encrypt symmetric key
-    QCA::MemoryRegion secureSymKey = pubKey.encrypt(symKey, QCA::EME_PKCS1_OAEP);
-
-    // Populate contents of new QVariantMap; may need to extract some fields
-    QVariantMap secureMsg;
-    secureMsg["secureSymKey"] = secureSymKey.toByteArray();
-    secureMsg["secureDatagram"] = dataMemOut.toByteArray();
-    secureMsg["secureDigest"] = secureDigest.toByteArray();
-    secureMsg["iV"] = iV.toByteArray();
-    
-    // Reconvert to byte array/datagram
-    QByteArray secureDatagram;
-    QDataStream outStream(&secureDatagram, QIODevice::WriteOnly);
-    outStream << secureMsg;
-
-    return secureDatagram;
-}
-
+// Takes in a QVariantMap DATA and returns a QByteArray of the encrypted map. 
+// Original data is encrypted using symmetric key with AES-128 cipher. 
+// Symmetric key is encrypted by recipient's public key using RSA-1028. Digest 
+// of original data is hashed using SHA-1 and signed with RSA-1028 using own 
+// private key. Initializtion vector for AES cipher is unsecured.
+// Params:
+//   data (QVariantMap): map to send encrypted
+//   pubKey (QCA::PublicKey): public key of recipient
+//   privKey (QCA::PrivateKey): private key of sender
+// Returns:
+//   (QByteArray): byte array of encrypted map
 QByteArray encryptMap(QVariantMap data, QCA::PublicKey pubKey, QCA::PrivateKey privKey)
 {
     // Initialize encryption tools
@@ -116,19 +85,11 @@ QByteArray encryptMap(QVariantMap data, QCA::PublicKey pubKey, QCA::PrivateKey p
     stream << data;
 
     // Encrypt data
-    QCA::SecureArray bufferBlock  = cipher.update(buffer);
+    QCA::SecureArray encryptedData = cipher.process(buffer);
     if (!cipher.ok()) {
-        qDebug() << "encryptMap: cipher.update(buffer) failed\n";
+        qDebug() << "encryptMap: cipher.process(buffer) failed\n";
         return newData; // TODO: Figure out what to return on fail
     }
-    QCA::SecureArray finalBlock = cipher.final();
-    if (!cipher.ok()) {
-        qDebug() << "encryptMap: cipher.final() failed\n";
-        return newData; // TODO: Figure out what to return on fail
-    }
-    // TODO: Unsure if .final() returns complete input data or just last block
-    // , for now assuming the latter
-    QCA::SecureArray encryptedData = bufferBlock.append(finalBlock);
 
     // Create signature
     QCA::SecureArray dataDigest = QCA::Hash("sha1").hash(buffer);
@@ -187,49 +148,17 @@ QVariantMap decryptMap(QByteArray datagram, QCA::PublicKey pubKey, QCA::PrivateK
     QCA::SecureArray decryptedData = cipher.process(data["encryptedData"]);
     if (!cipher.ok()) {
         qDebug() << "decryptData: cipher.update() failed\n";
-        return newData; // TODO: Figure out what to return on fail
+        data["success"] = false;
+        return data;
     }
 
+    // Check signature
+    if (!pubKey.verifyMessage(decryptedData, data["signature"], QCA::EMSA1_SHA1);) {
+        qDebug() << "decryptData: veryMessage() failed\n";
+        data["success"] = false;
+        return data;
+    }
+
+    // TODO: STOPPED 11/14/14 00:42. NEED TO RETURN THE DECRYPTED MAP NOW. GOT ALL THE CONTESNT READY.
+
 };
-
-QByteArray decryptDatagram(QByteArray secureMsg, QCA::PublicKey pubKey, QCA::PrivateKey privKey) 
-{
-    // Deserialize the secureMsg and its contents
-    QDataStream *secureMsgStream = new QDataStream(&secureMsg, QIODevice::ReadOnly);
-    QVariantMap msg;
-    *secureMsgStream >> msg;
-
-    QDataStream *secureSymKeyStream = new QDataStream(&(msg["secureSymKey"]), QIODevice::ReadOnly);
-    QCA::MemoryRegion secureSymKey;
-    *secureSymKeyStream >> secureSymKey;
-
-    QDataStream *secureDatagramStream= new QDataStream(&(msg["secureDatagram"]), QIODevice::ReadOnly);
-    QCA::MemoryRegion secureDatagram;
-    *secureDatagramStream >> secureDatagram;
-
-    QDataStream *secureDigestStream = new QDataStream(&(msg["secureDigest"]), QIODevice::ReadOnly);
-    QCA::MemoryRegion secureDigest;
-    *secureDigestStream >> secureDigest;
-
-    QDataStream *iVStream = new QDataStream(&(msg["iV"]), QIODevice::ReadOnly);
-    QCA::InitializationVector iV;
-    *iVStream >> iV;
-
-    // Decrypt random key with private key (RSA)
-    QCA::SecureArray symKey;
-    privKey.decrypt(QCA::MemoryRegion(secureSymKey), &symKey, QCA::EME_PKCS1_OAEP);
-
-    // Decrypt message with random key (symmetric)
-    QCA::Cipher cipher(QString("aes128"), QCA::Cipher::CBC,
-        QCA::Cipher::DefaultPadding, QCA::Decode, symKey, iV);
-
-    QCA::MemoryRegion datagram;
-    datagram = cipher.update(secureDatagram);
-    datagram = cipher.final();
-
-    // Verify digest with public key of sender (RSA)
-    QByteArray myDigest = QCA::Hash("sha1").hash(datagram).toByteArray();
-    QByteArray digest = pubKey.encrypt(secureDigest, QCA::EME_PKCS1_OAEP);
-
-    return datagram.toByteArray();
-}
